752b76c88c320726237b7c7f85d421ed5a3d0724
pernik85 <pernik2@mail.ru>
2016-09-28 20:05:02 +0300

    Update PgIntegerArrayValidatorTest.php
    add tests function validate

--- tests/PgIntegerArrayValidatorTest.php
+++ tests/PgIntegerArrayValidatorTest.php
@@ -78,4 +78,69 @@ class PgIntegerArrayValidatorTest extends PHPUnit_Framework_TestCase{
     }
 
 
+    /**
+     * @dataProvider provider_validate_badParams
+     */
+    public function test_validate_badParams($val, $exceptionMessage){
+
+        $this->PgIntegerArrayValidatorMock->expects($this->once())
+            ->method('isEmpty')
+            ->with($this->equalTo($val))
+            ->will($this->returnValue(false));
+
+        $this->setExpectedException('Exception', $exceptionMessage);
+
+       $this->PgIntegerArrayValidatorMock->validate($val);
+    }
+
+
+    public function provider_validate_badParams(){
+        return array(
+            array(159, 'не являеится массивом'),
+            array(array(0, 15, '91'), 'массив содержит не верные значение'),
+        );
+    }
+
+    /**
+     * @dataProvider provider_validate_goodParams
+     */
+    public function test_validate_goodParams($val){
+
+        $this->PgIntegerArrayValidatorMock->expects($this->once())
+            ->method('isEmpty')
+            ->with($this->equalTo($val))
+            ->will($this->returnValue(false));
+
+
+       $this->PgIntegerArrayValidatorMock->validate($val);
+    }
+
+    public function provider_validate_goodParams(){
+        return array(
+            array(array(159, 7)),
+            array(array(0, 15, 91)),
+        );
+    }
+
+    /**
+     * @dataProvider provider_validate_emptyParams
+     */
+    public function test_validate_emptyParams($val){
+
+        $this->PgIntegerArrayValidatorMock->expects($this->once())
+            ->method('isEmpty')
+            ->with($this->equalTo($val))
+            ->will($this->returnValue(true));
+
+
+       $this->PgIntegerArrayValidatorMock->validate($val);
+    }
+
+    public function provider_validate_emptyParams(){
+        return array(
+            array(array()),
+            array(''),
+        );
+    }
+
 }
